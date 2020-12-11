77c28834dccbe58fb31efda44194646c5ba71d6e
Dmytro Khomenko <d.khomenko@ukma.edu.ua>
2020-12-11 13:41:36 +0200

    Prettier

--- src/App.js
+++ src/App.js
@@ -1,57 +1,43 @@
-import React, {Component} from 'react';
-import {withAuthenticator} from 'aws-amplify-react';
-
-
+import React, { Component } from "react";
+import { withAuthenticator } from "aws-amplify-react";
 
 class App extends Component {
-  state = { 
+  state = {
     notes: [
       {
-        id : 1,
-        note: "hello world"
-      }
-    ] 
-  }
-  render(){
-    const {notes} = this.state
-    return ( <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
-      <h1 className="code f2-l">Amplify Notetaker</h1>
-      { /*Note Form*/ }
-      <form className="mb3">
-        <input 
-        type="text" 
-        className="pa2 f4" 
-        placeholder= "Write your note"/> 
-        <button className="pa2 f4"
-          type = "submit">
+        id: 1,
+        note: "hello world",
+      },
+    ],
+  };
+  render() {
+    const { notes } = this.state;
+    return (
+      <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
+        <h1 className="code f2-l">Amplify Notetaker</h1>
+        {/*Note Form*/}
+        <form className="mb3">
+          <input type="text" className="pa2 f4" placeholder="Write your note" />
+          <button className="pa2 f4" type="submit">
             Add note
-        </button>
-      </form>
-      { /*Notes list*/ }
-      <div>
-         {notes.map(item =>(
-           <div key = {item.id}
-           className="flex-items-center">
-             <li
-             className="list pa1 f3"
-             >
-               {item.note}
-            <button 
-            className="bg-transparent bn f4"
-            >
-              
-
-              <span>&times;</span>
-            </button> 
-            </li>
-
-
-           </div>
-         ) )}
+          </button>
+        </form>
+        {/*Notes list*/}
+        <div>
+          {notes.map((item) => (
+            <div key={item.id} className="flex-items-center">
+              <li className="list pa1 f3">
+                {item.note}
+                <button className="bg-transparent bn f4">
+                  <span>&times;</span>
+                </button>
+              </li>
+            </div>
+          ))}
+        </div>
       </div>
-   
-    </div> );
+    );
   }
 }
 
-export default withAuthenticator(App, {includeGreetings: true});
+export default withAuthenticator(App, { includeGreetings: true });
